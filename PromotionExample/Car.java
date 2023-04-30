0eae60ca195b390385a7c7773285c360aa91070f
kimjieung97 <dickseung23@naver.com>
2023-04-30 19:08:04 +0900

    # 다형성 예제 (객체배열 관리 코드 간략화)

--- PromotionExample/Car.java
+++ PromotionExample/Car.java
@@ -3,19 +3,32 @@ package PromotionExample;
 public class Car {
 
     //필드
-    Tire frontLeftTire = new Tire(6,"앞 왼쪽");
-    Tire frontRightTire = new Tire(2,"앞 오른쪽");
-    Tire backLeftTire = new Tire(3,"뒤왼쪽");
-    Tire backRightTire = new Tire(4,"뒤 오른쪽");
+//    Tire frontLeftTire = new Tire(6,"앞 왼쪽");
+//    Tire frontRightTire = new Tire(2,"앞 오른쪽");
+//    Tire backLeftTire = new Tire(3,"뒤왼쪽");
+//    Tire backRightTire = new Tire(4,"뒤 오른쪽");
+
+    Tire[] tires = {
+            new Tire(6,"앞 왼쪽"),
+            new Tire(2,"앞 오른쪽"),
+            new Tire(3,"뒤왼쪽"),
+            new Tire(4,"뒤 오른쪽")
+    };
     //생성자
 
     //메소드
     int run(){
         System.out.println("자동차가 달립니다");
-        if(frontLeftTire.roll()==false){stop(); return 1;}
-        if(frontRightTire.roll()==false){stop(); return 2;}
-        if(backLeftTire.roll()==false){stop(); return 3;}
-        if(backRightTire.roll()==false){stop(); return 4;}
+//        if(frontLeftTire.roll()==false){stop(); return 1;}
+//        if(frontRightTire.roll()==false){stop(); return 2;}
+//        if(backLeftTire.roll()==false){stop(); return 3;}
+//        if(backRightTire.roll()==false){stop(); return 4;}
+        for (int i = 0; i <tires.length ; i++) {
+            if(tires[i].roll()==false){
+                stop();
+                return (i+1);
+            }
+        }
         return 0;
     }
 
--- PromotionExample/CarExample.java
+++ PromotionExample/CarExample.java
@@ -7,23 +7,27 @@ public class CarExample {
         for(int i =1; i<=5; i++){
             int problemLocation = car.run();
 
-            switch (problemLocation){
-                case 1:
-                    System.out.println("앞왼쪽 한국타이어로 교체");
-                    car.frontLeftTire = new HankookTire(15,"앞 왼쪽");
-                    break;
-                case 2:
-                    System.out.println("앞 오른쪽 금호 타이어로 교체");
-                    car.frontRightTire = new KumhoTire(13,"앞 오른쪽");
-                    break;
-                case 3:
-                    System.out.println("뒤 왼쪽 한국 타이어로 교체");
-                    car.backLeftTire = new HankookTire(14,"뒤 왼쪽");
-                    break;
-                case 4:
-                    System.out.println("뒤 오른쪽 타이어 금호 타이어로 교체");
-                    car.backRightTire = new KumhoTire(17, "뒤 오른쪽");
-                    break;
+//            switch (problemLocation){
+//                case 1:
+//                    System.out.println("앞왼쪽 한국타이어로 교체");
+//                    car.frontLeftTire = new HankookTire(15,"앞 왼쪽");
+//                    break;
+//                case 2:
+//                    System.out.println("앞 오른쪽 금호 타이어로 교체");
+//                    car.frontRightTire = new KumhoTire(13,"앞 오른쪽");
+//                    break;
+//                case 3:
+//                    System.out.println("뒤 왼쪽 한국 타이어로 교체");
+//                    car.backLeftTire = new HankookTire(14,"뒤 왼쪽");
+//                    break;
+//                case 4:
+//                    System.out.println("뒤 오른쪽 타이어 금호 타이어로 교체");
+//                    car.backRightTire = new KumhoTire(17, "뒤 오른쪽");
+//                    break;
+//            }
+            if(problemLocation!=0){
+                System.out.println(car.tires[problemLocation-1].location + "한국타이어로 교체");
+                car.tires[problemLocation-1] = new HankookTire(15,car.tires[problemLocation-1].location);
             }
             System.out.println("------------------------------------------"); // 1회전시 출력되는 내용 구분
         }
