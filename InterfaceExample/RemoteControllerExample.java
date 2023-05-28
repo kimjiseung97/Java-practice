8878f553294aebed84540867bafebfdc21ee1258
kimjieung97 <dickseung23@naver.com>
2023-05-29 00:43:59 +0900

    # 중첩클래스 예제

---
+++ InterfaceExample/RemoteControllerExample.java
@@ -0,0 +1,17 @@
+package InterfaceExample;
+
+public class RemoteControllerExample {
+    public static void main(String[] args) {
+        RemoteControl rc = null;
+
+        rc = new Television();
+
+        rc.turnOn();
+        rc.turnOff();
+
+        rc = new Audio();
+
+        rc.turnOn();
+        rc.turnOff();
+    }
+}
