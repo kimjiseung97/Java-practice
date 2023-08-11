ecf798dd9839d3264515446cab3857cc8535cc82
jisuengkim <dickseung23@naver.com>
2023-08-11 17:43:30 +0900

    # string 클래스 메서드 활용

---
+++ stringClassExample/StringManipulation.java
@@ -0,0 +1,34 @@
+package stringClassExample;
+
+public class StringManipulation {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        String str = "HelloWorld";
+
+
+        String s = str.replaceAll("o", "X");
+        //값을 변경하더라도 원본의 str주소의 값은 바뀌지않는다
+
+        char c = str.charAt(0);
+
+        System.out.println(str.length());
+
+        System.out.println(s);
+
+        System.out.println(str.toLowerCase());
+
+        System.out.println(str.toUpperCase());
+
+        System.out.println(c);
+
+        System.out.println(str.substring(0,4));
+
+        System.out.println(str.indexOf("d"));
+
+        for (int i = 0; i <str.length() ; i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+}
