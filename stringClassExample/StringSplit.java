d40f6ff3627acd9851ede1253b9e65aba4066be1
jisuengkim <dickseung23@naver.com>
2023-08-11 18:23:08 +0900

    # string split 메서드

---
+++ stringClassExample/StringSplit.java
@@ -0,0 +1,26 @@
+package stringClassExample;
+
+import java.util.Scanner;
+
+public class StringSplit {
+    public static void main(String[] args) {
+
+        String str = "Hello World  Java";
+
+       // String[] split = strArray.split(",");
+        String[] strArray = str.split("\\s+");
+        for (String s : strArray) {
+            System.out.println(s);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        String next = scanner.next();
+
+        String[] splitArray = next.split("s");
+
+        for (String s : splitArray) {
+            System.out.println(s);
+        }
+    }
+}
