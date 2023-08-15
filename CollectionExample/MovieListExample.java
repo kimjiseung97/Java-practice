430a77c74610b63f1304367038813e19d103c5bb
jisuengkim <dickseung23@naver.com>
2023-08-15 22:21:18 +0900

    # colletionList ArrayList 중복이가능한 단순 ArrayList 이진탐색 예제

--- CollectionExample/MovieListExample.java
+++ CollectionExample/MovieListExample.java
@@ -3,6 +3,7 @@ package CollectionExample;
 import java.util.ArrayList;
 
 public class MovieListExample {
+
     public static void main(String[] args) {
         ArrayList<Movie> list = new ArrayList<>(); //Movie 객체의 배열 생성 따로 제네릭타입 설정안하면 오브젝트타입으로 가기때문에 업 다운 케스팅필요
 
@@ -12,6 +13,15 @@ public class MovieListExample {
 
         for (Movie movie : list) {
             System.out.println(movie);
+        }
+
+        String searchTitle = "기생충";
+        //순차검색 -> 이진검색
+        for (Movie movie : list) {
+            if (movie.getTitle().equals(searchTitle)){
+                System.out.println(movie.toString());
+                break;
+            }
         }
 
     }
